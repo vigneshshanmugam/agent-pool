2bf8c78f6f817032021a6e911d254d8728a64bb9
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2018-09-20 21:00:12 +0200

    add tests for preconnect and fix mocha

--- test/agent-pool.test.js
+++ test/agent-pool.test.js
@@ -2,11 +2,12 @@
 
 const assert = require("assert");
 const sinon = require("sinon");
-const HttpsAgent = require("agentkeepalive").HttpsAgent;
+const http = require("http");
+const Agent = require("agentkeepalive");
 const AgentPool = require("../lib/agent-pool");
 
 describe("AgentPool", () => {
-  let httpsPool, loggerStub, clock;
+  let httpPool, loggerStub, clock;
   let destroyTime = 1000;
 
   beforeEach(() => {
@@ -16,9 +17,10 @@ describe("AgentPool", () => {
       info: sinon.spy(),
       warn: sinon.spy()
     };
-    httpsPool = new AgentPool({
+    httpPool = new AgentPool({
       maxAgents: 3,
       destroyTime,
+      agentType: Agent,
       logger: loggerStub
     });
   });
@@ -28,8 +30,8 @@ describe("AgentPool", () => {
   });
 
   it("create pool with agent options", () => {
-    assert.equal(httpsPool.maxAgents, 3);
-    assert.equal(httpsPool.destroyTime, 1000);
+    assert.equal(httpPool.maxAgents, 3);
+    assert.equal(httpPool.destroyTime, 1000);
   });
 
   it("return https agent with specified options", () => {
@@ -45,41 +47,184 @@ describe("AgentPool", () => {
     assert.equal(agent.maxSockets, 20);
   });
 
-  it("return https agent by default", () => {
-    assert(httpsPool.getAgent() instanceof HttpsAgent);
+  it("return the agent instance that is passed to agentType", () => {
+    assert(httpPool.getAgent() instanceof Agent);
   });
 
   it("return the old active agent for successive calls", () => {
-    const agent1 = httpsPool.getAgent();
-    const agent2 = httpsPool.getAgent();
+    const agent1 = httpPool.getAgent();
+    const agent2 = httpPool.getAgent();
     assert.deepStrictEqual(agent1, agent2);
-    assert.equal(httpsPool.agents.length, 1); // Number of agents on the pool
+    assert.equal(httpPool.agents.length, 1); // Number of agents on the pool
   });
 
   it("return the new active agent for same pool after agent destroy time", () => {
-    const agent1 = httpsPool.getAgent();
+    const agent1 = httpPool.getAgent();
     clock.tick(destroyTime);
-    const agent2 = httpsPool.getAgent();
+    const agent2 = httpPool.getAgent();
     assert.notDeepStrictEqual(agent1, agent2);
-    assert.equal(httpsPool.agents.length, 2);
+    assert.equal(httpPool.agents.length, 2);
   });
 
   it("should recycle old agent after 3 max agents and timeout", () => {
-    httpsPool.getAgent();
+    httpPool.getAgent();
     clock.tick(destroyTime);
-    httpsPool.getAgent();
-    assert.equal(httpsPool.agents.length, 2);
+    httpPool.getAgent();
+    assert.equal(httpPool.agents.length, 2);
     clock.tick(destroyTime);
-    httpsPool.getAgent();
-    assert.equal(httpsPool.agents.length, 3);
+    httpPool.getAgent();
+    assert.equal(httpPool.agents.length, 3);
     // to make sure its not more than 3 agents
-    httpsPool.getAgent();
-    assert.equal(httpsPool.agents.length, 3);
+    httpPool.getAgent();
+    assert.equal(httpPool.agents.length, 3);
     // to trigger recycle
     clock.tick(destroyTime);
-    assert.equal(httpsPool.agents.length, 2);
-    // trigger to create new agent since the httpsPool is down by 1
+    assert.equal(httpPool.agents.length, 2);
+    // trigger to create new agent since the httpPool is down by 1
     clock.tick(destroyTime);
-    assert.equal(httpsPool.agents.length, 3);
+    assert.equal(httpPool.agents.length, 3);
+  });
+
+  it("return metrics for active agent when changed", () => {
+    const httpRequest = new http.ClientRequest("http://localhost:20/");
+
+    const agent = httpPool.getAgent();
+    agent.addRequest(httpRequest, {});
+
+    httpRequest.on("error", () => {});
+    httpRequest.end();
+
+    const { createSocketCount, sockets } = httpPool.stats();
+
+    assert.deepStrictEqual(
+      { createSocketCount, sockets },
+      {
+        createSocketCount: 1,
+        sockets: { "localhost::": 1 }
+      }
+    );
+    // successive call to stats should return null
+    assert.equal(null, httpPool.stats());
+  });
+
+  describe("Agent with preconnected sockets", () => {
+    let server = null;
+    let port = null;
+    before(done => {
+      server = http.createServer((req, res) => {
+        return res.end(req.url);
+      });
+      server.listen(0, () => {
+        port = server.address().port;
+        done();
+      });
+    });
+
+    function requestPromise(options) {
+      return new Promise((resolve, reject) => {
+        const request = http.request(options);
+
+        request.on("response", resolve);
+        request.on("error", reject);
+        request.end();
+      });
+    }
+
+    it("handle socket connection failures during preconnect", done => {
+      const options = {
+        agent: httpPool.getAgent(),
+        port: 2, // unassigned port
+        path: "/"
+      };
+      const agent1 = httpPool.getAgent();
+
+      requestPromise(options)
+        .catch(() => {
+          assert.equal(httpPool.agents.length, 1);
+          clock.tick(destroyTime);
+          sinon.assert.calledWithExactly(
+            loggerStub.info,
+            "Creating Preconnect agent with socket pool of 1"
+          );
+          return requestPromise(options);
+        })
+        .catch(() => {
+          clock.tick(destroyTime / 3);
+          const agent2 = httpPool.getAgent();
+          assert.equal(httpPool.agents.length, 2);
+          assert.notStrictEqual(agent1, agent2);
+
+          const { sockets, freeSockets } = httpPool.stats();
+          assert.deepStrictEqual(
+            { sockets, freeSockets },
+            {
+              sockets: {},
+              freeSockets: {}
+            }
+          );
+          done();
+        });
+    });
+
+    it("create agent with preconnect sockets", done => {
+      const options = {
+        agent: httpPool.getAgent(),
+        port,
+        path: "/"
+      };
+      const agent1 = httpPool.getAgent();
+
+      requestPromise(options)
+        .then(() => {
+          assert.equal(httpPool.agents.length, 1);
+          const socketNames = Object.keys(agent1.sockets);
+          assert.deepStrictEqual(socketNames, [`localhost:${port}:`]);
+
+          let { createSocketCount, sockets } = httpPool.stats();
+          assert.deepStrictEqual(
+            { createSocketCount, sockets },
+            {
+              createSocketCount: 1,
+              sockets: { [`localhost:${port}:`]: 1 }
+            }
+          );
+          clock.tick(destroyTime);
+          sinon.assert.calledWith(
+            loggerStub.info,
+            "Creating Preconnect agent with socket pool of 1"
+          );
+          return requestPromise(options);
+        })
+        .then(() => {
+          // Activate new preconnect agent
+          clock.tick(destroyTime / 3);
+          const agent2 = httpPool.getAgent();
+          assert.equal(httpPool.agents.length, 2);
+          assert.notStrictEqual(agent1, agent2);
+          // sockets will be assigned as freeSockets for new agents
+          const { createSocketCount, sockets, freeSockets } = httpPool.stats();
+
+          assert.deepStrictEqual(
+            { createSocketCount, sockets, freeSockets },
+            {
+              createSocketCount: 1,
+              sockets: {},
+              freeSockets: { [`localhost:${port}:`]: 1 }
+            }
+          );
+
+          clock.tick(destroyTime);
+          /**
+           * There are no requests in flight to trigger movement of freeSockets -> sockets so we should
+           * create agent without preconnect sockets
+           */
+          sinon.assert.calledWith(
+            loggerStub.debug,
+            "Creating agent without preconnected sockets"
+          );
+          assert.equal(httpPool.agents.length, 3);
+          done();
+        });
+    });
   });
 });
