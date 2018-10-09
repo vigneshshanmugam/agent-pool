43aa787f05f7761e3eb6c99bfae96024dbc73183
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2018-10-09 15:50:21 +0200

    fix formating in types

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,34 @@
-import { AgentStatus } from 'agentkeepalive'
-import { AgentOptions, Agent } from 'http';
+import { AgentStatus } from "agentkeepalive";
+import { AgentOptions, Agent } from "http";
 
 type LogFn = (message?: any, ...optional: any[]) => void;
 
 interface Logger {
-    info: LogFn;
-    warn: LogFn;
-    error: LogFn;
-    debug: LogFn;
+  info: LogFn;
+  warn: LogFn;
+  error: LogFn;
+  debug: LogFn;
 }
 
 export interface AgentPoolOptions {
-    agentType?: Agent;
-    maxAgents?: number;
-    destroyTime?: number;
-    logger?: Logger;
+  agentType?: Agent;
+  maxAgents?: number;
+  destroyTime?: number;
+  logger?: Logger;
 }
-    
+
 declare class AgentPool {
-    constructor(options? : AgentPoolOptions, agentOptions?: AgentOptions) {}
+  constructor(options?: AgentPoolOptions, agentOptions?: AgentOptions) {}
 
-    /**
-     * Get active agent from the pool
-     */
-    getAgent(): Agent;
+  /**
+   * Get active agent from the pool
+   */
+  getAgent(): Agent;
 
-    /**
-     * Get the stats for the pool at any given point of time
-     */
-    stats(): AgentStatus | null;
+  /**
+   * Get the stats for the pool at any given point of time
+   */
+  stats(): AgentStatus | null;
 }
 
 export = AgentPool;
